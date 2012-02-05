4a93563e297d539832ccf46a12a9f49bca81e131
sonicxml <sonicxml@gmail.com>
2012-02-04 20:55:18 -0500

    add lulzactive governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -370,6 +370,9 @@ extern struct cpufreq_governor cpufreq_gov_lagfree;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG)
 extern struct cpufreq_governor cpufreq_gov_hotplug;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hotplug)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVE)
+extern struct cpufreq_governor cpufreq_gov_lulzactive;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lulzactive)
 #endif
 
 
