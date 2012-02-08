69e46f231507da729f704b4fa40effe895c5646e
sonicxml <sonicxml@gmail.com>
2012-02-08 17:46:21 -0500

    some more conservative gov mods - this should cause it to jump up a huge amount less

--- drivers/cpufreq/cpufreq_conservative.c
+++ drivers/cpufreq/cpufreq_conservative.c
@@ -32,8 +32,8 @@ static unsigned int enabled = 0;
  * It helps to keep variable names smaller, simpler
  */
 
-#define DEF_FREQUENCY_UP_THRESHOLD		(67)
-#define DEF_FREQUENCY_DOWN_THRESHOLD		(20)
+#define DEF_FREQUENCY_UP_THRESHOLD		(60)
+#define DEF_FREQUENCY_DOWN_THRESHOLD		(30)
 
 /*
  * The polling frequency of this governor depends on the capability of
@@ -95,7 +95,7 @@ static struct dbs_tuners {
 	.down_threshold = DEF_FREQUENCY_DOWN_THRESHOLD,
 	.sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR,
 	.ignore_nice = 0,
-	.freq_step = 50,
+	.freq_step = 30,
 };
 
 static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
