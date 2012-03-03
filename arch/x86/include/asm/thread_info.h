00f1c437f80a97c92a2e85487d9fe1d75959f5c5
jdkoreclipse <jdkoreclipse@gmail.com>
2012-03-03 12:20:22 -0500

    Revert "Revert "get rid of old jrcu commit i rorder to use the official one""
    This reverts commit a9e1e7dea7f12b8c858e5faeb23a762604ce4077.

--- arch/x86/include/asm/thread_info.h
+++ arch/x86/include/asm/thread_info.h
@@ -29,7 +29,6 @@ struct thread_info {
 	__u32			flags;		/* low level flags */
 	__u32			status;		/* thread synchronous flags */
 	__u32			cpu;		/* current CPU */
-#define HAVE_THREAD_INFO_CPU   1
 	int			preempt_count;	/* 0 => preemptable,
 						   <0 => BUG */
 	mm_segment_t		addr_limit;
