1b4773afb6a8815649cfbde993533fd6949eaff9
sonicxml <sonicxml@gmail.com>
2012-03-25 12:11:00 -0400

    change this to 1.06ghz

--- arch/arm/mach-omap2/dpll44xx.c
+++ arch/arm/mach-omap2/dpll44xx.c
@@ -192,7 +192,7 @@ int omap4_prcm_freq_update(void)
 
 #define OMAP_1_5GHz	1500000000
 #define OMAP_1_2GHz	1200000000
-#define OMAP_1GHz	1000000000
+#define OMAP_1GHz	1060000000
 #define OMAP_920MHz	920000000
 #define OMAP_748MHz	748000000
 
