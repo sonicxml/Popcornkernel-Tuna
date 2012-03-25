4bb5841be7dbc2c91c8425432a2c3b615ffb325e
sonicxml <sonicxml@gmail.com>
2012-03-24 20:29:06 -0400

    remove ezekeel deepidle

--- drivers/remoteproc/omap_remoteproc.c
+++ drivers/remoteproc/omap_remoteproc.c
@@ -545,11 +545,6 @@ err:
 
 static int omap_rproc_set_lat(struct rproc *rproc, long val)
 {
-#ifdef CONFIG_OMAP_IPU_DEEPIDLE
-	if (val == 40)
-	    val = 1500;
-#endif
-
 	pm_qos_update_request(rproc->qos_request, val);
 	return 0;
 }
