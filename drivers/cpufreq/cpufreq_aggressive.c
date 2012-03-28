4adff1e9055bc94a80d6b549f284e28caf71e41b
sonicxml <sonicxml@gmail.com>
2012-03-27 20:19:15 -0400

    upon further testing, 85 seems to be fine

--- drivers/cpufreq/cpufreq_aggressive.c
+++ drivers/cpufreq/cpufreq_aggressive.c
@@ -485,7 +485,7 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 				CPUFREQ_RELATION_H);
 		return;
 	}
-	if (max_load < 87 && this_dbs_info->requested_freq == 1200000)	
+	if (max_load < 85 && this_dbs_info->requested_freq == 1200000)	
 		__cpufreq_driver_target(policy, 1060000,
 				CPUFREQ_RELATION_H);
 }
