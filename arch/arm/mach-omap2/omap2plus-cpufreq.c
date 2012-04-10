1846d3490d3c98eff14e997f8e71df16b3d37c34
sonicxml <sonicxml@gmail.com>
2012-04-10 17:12:43 -0400

    lets try lowering this to 10 and see how it goes - from KalimochoAz

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -360,7 +360,7 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 		cpumask_setall(policy->cpus);
 	}
 
-	policy->cpuinfo.transition_latency = 30 * 1000;
+	policy->cpuinfo.transition_latency = 10 * 1000;
 
 	return 0;
 
