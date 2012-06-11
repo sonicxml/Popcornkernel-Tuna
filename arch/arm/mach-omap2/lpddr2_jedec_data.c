3869f4903587c275dee785636a0298cd9938fbc6
sonicxml <sonicxml@gmail.com>
2012-06-10 22:58:19 -0400

    RAM Overclock

--- arch/arm/mach-omap2/lpddr2_jedec_data.c
+++ arch/arm/mach-omap2/lpddr2_jedec_data.c
@@ -39,7 +39,7 @@ const struct lpddr2_addressing lpddr2_jedec_addressing_table[] = {
  * timings are not available from the device data sheet.
  */
 const struct lpddr2_timings lpddr2_jedec_timings_400_mhz = {
-	.max_freq	= 400000000,
+	.max_freq	= 420000000,
 	.RL		= 6,
 	.tRPab		= 21,
 	.tRCD		= 18,
@@ -63,7 +63,7 @@ const struct lpddr2_timings lpddr2_jedec_timings_400_mhz = {
 
 /* Base AC Timing values specified by JESD209-2 for 333 MHz operation */
 const struct lpddr2_timings lpddr2_jedec_timings_333_mhz = {
-	.max_freq	= 333000000,
+	.max_freq	= 349650000,
 	.RL		= 5,
 	.tRPab		= 21,
 	.tRCD		= 18,
@@ -87,7 +87,7 @@ const struct lpddr2_timings lpddr2_jedec_timings_333_mhz = {
 
 /* Base AC Timing values specified by JESD209-2 for 200 MHz operation */
 const struct lpddr2_timings lpddr2_jedec_timings_200_mhz = {
-	.max_freq	= 200000000,
+	.max_freq	= 205000000,
 	.RL		= 3,
 	.tRPab		= 21,
 	.tRCD		= 18,
