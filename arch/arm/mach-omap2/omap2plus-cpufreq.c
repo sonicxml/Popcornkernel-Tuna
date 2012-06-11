05c45ea644ccd8a4c0bb093ad6f4d4d8fdd5d7aa
sonicxml <sonicxml@gmail.com>
2012-06-10 23:01:36 -0400

    omap2plus-cpufreq.c: fix for TF doesn't work when CPU1 on/off - from Linaro

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -359,8 +359,7 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 
 	for (i = 0; freq_table[i].frequency != CPUFREQ_TABLE_END; i++)
 		max_freq = max(freq_table[i].frequency, max_freq);
-	max_thermal = max_freq;
-
+	
 	/*
 	 * On OMAP SMP configuartion, both processors share the voltage
 	 * and clock. So both CPUs needs to be scaled together and hence
@@ -768,7 +767,8 @@ static int __init omap_cpufreq_init(void)
 
 	ret = cpufreq_register_driver(&omap_driver);
 	omap_cpufreq_ready = !ret;
-
+	
+	max_thermal = max_freq;
 	if (!ret) {
 		int t;
 
